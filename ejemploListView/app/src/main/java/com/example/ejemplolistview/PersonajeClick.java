311340d75ff4c3d23b5c5d5cfc47bbad60f30402
Z3nitt <helios1612@gmail.com>
2024-11-05 14:18:01 +0100

    cambios

--- ejemploListView/app/src/main/java/com/example/ejemplolistview/PersonajeClick.java
+++ ejemploListView/app/src/main/java/com/example/ejemplolistview/PersonajeClick.java
@@ -1,6 +1,7 @@
 package com.example.ejemplolistview;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PersonajeClick extends AppCompatActivity {
-    private Button btnatras;
+    private Button btnatras, btnvideo, btncoords, btntlf, btnweb;
     private ImageView ivfoto;
     private TextView txtnombre,txtdesc;
 
@@ -27,11 +28,50 @@ public class PersonajeClick extends AppCompatActivity {
         ivfoto = (ImageView) findViewById(R.id.foto);
         txtnombre = (TextView) findViewById(R.id.nombre);
         txtdesc = (TextView) findViewById(R.id.desc);
+        btnvideo = (Button) findViewById(R.id.btnvideo);
+        btncoords = (Button) findViewById(R.id.btncoords);
+        btntlf = (Button) findViewById(R.id.btntlf);
+        btnweb = (Button) findViewById(R.id.btnweb);
 
         Intent intent = getIntent();
         String nombre = intent.getStringExtra("nombre");
         String descExtendida = intent.getStringExtra("descExtendida");
         int imagenId = intent.getIntExtra("imagenId", -1);
+
+        btnweb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentweb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+                startActivity(intentweb);
+
+            }
+        });
+
+        btnvideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentvideo = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+                startActivity(intentvideo);
+            }
+        });
+
+        btncoords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentcoords = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=1600 Amphitheatre Parkway, Mountain View, CA"));
+                startActivity(intentcoords);
+            }
+        });
+
+        btntlf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intenttlf = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456789"));
+                startActivity(intenttlf);
+            }
+        });
+
+
 
         txtnombre.setText(nombre);
         txtdesc.setText(descExtendida);
